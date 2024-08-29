3c59e3bb42b75d20774528f0bbcd5cc7ee22b1c6
dxg6620 <dxg6620@mavs.uta.edu>
2024-08-28 22:42:10 -0500

    Create insertionSort.ts

---
+++ insertionSort.ts
@@ -0,0 +1,24 @@
+const insertionSort = (arr? : number[]): number[] | void => {
+
+  if (!arr) {
+    return;
+  }
+
+  if (arr.length > 1) {
+    for (let i = 1; i < arr.length; i++) {
+      let swapValue = arr[i];
+      let j = i - 1;
+
+      while (j >= 0 && arr[j] > swapValue) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+			arr[j+1] = swapValue;
+    }
+		return arr;
+  } else {
+    return arr;
+  }
+};
+
+console.log(insertionSort([10, 20, -1, 2,1, 5,5,10])
