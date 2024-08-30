ecaaeecd401c9815a5410118b6e9cf2725826960
dxg6620 <dxg6620@mavs.uta.edu>
2024-08-30 11:11:32 -0500

    Create bubbleSort.ts

---
+++ bubbleSort.ts
@@ -0,0 +1,13 @@
+const bubbleSort = (arr: number[]): number[] => {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([10, 1, 2, 40, -1, 2, 50, 23]))
