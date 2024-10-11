aa8fc7577aeefbd712ce24fa06bce29010c6e248
dxg6620 <dxg6620@mavs.uta.edu>
2024-10-10 21:49:54 -0500

    Create Queue.ts

---
+++ Queue.ts
@@ -0,0 +1,34 @@
+class Queue {
+  private arr: number[];
+  private front: number;
+  private rear: number;
+  private maxSize: number;
+
+  constructor(size: number) {
+    this.maxSize = size;
+    this.arr = new Array(this.maxSize);
+    this.front = -1;
+    this.rear = -1;
+  }
+
+  enqueue(value: number): void {
+    if (this.rear === this.maxSize - 1) {
+      throw new Error("Queue overflow");
+    }
+    if (this.front === -1) {
+      this.front = 0;
+    }
+    this.arr[++this.rear] = value;
+  }
+
+  dequeue(): number {
+    if (this.front === -1 || this.front > this.rear) {
+      throw new Error("Queue underflow");
+    }
+    return this.arr[this.front++];
+  }
+
+  isEmpty(): boolean {
+    return this.front === -1 || this.front > this.rear;
+  }
+}
